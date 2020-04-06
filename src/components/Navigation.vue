502b828b1986751ba77a64be15fe237f9bab2cfb
andriar.rizkiansah <andriar.rizkiansah@mavila.co.id>
2020-04-06 17:57:18 +0700

    first page mobile

---
+++ src/components/Navigation.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="nav">
+    <router-link class="child" to="/">Home</router-link>
+    <router-link class="child" to="/about">About</router-link>
+    <router-link class="child" to="/about">Shit !</router-link>
+  </div>
+  <!-- <div id="nav-mobile" justify-end>
+    <v-btn text large depressed>
+      <v-icon color="white">mdi-menu</v-icon>
+    </v-btn>
+  </div>-->
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Navigation extends Vue {
+  // @Prop() private msg!: string;
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <div class="home">
+      <Navigation />
+      <v-row class="box-page">
+        <template class="d-flex">
+          <v-col class="justify-xl-space-around">
+            <div class="nick-name">Andriar</div>
+            <div class="name">Mafazan Rizkiansah</div>
+            <div class="department">Software Engineer</div>
+            <div class="button-group">
+              <v-btn class="mr-2 mt-2" tile outlined color="#FFFFFF" small>
+                <v-icon small left>mdi-message</v-icon>Hire me !
+              </v-btn>
+              <v-btn class="mr-2 mt-2" tile outlined color="#FFFFFF" small>Portofolio</v-btn>
+            </div>
+          </v-col>
+          <!-- <v-col>
+            <div class="name">isi dari elu</div>
+          </v-col>-->
+        </template>
+      </v-row>
+      <div class="description">
+        <div>
+          Hi !, My name is Andriar. I have experience in working on team for
+          planning and executing a project using agile framework in developing
+          applications.
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import Navigation from "@/components/Navigation";
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Navigation
   }
 };
 </script>
